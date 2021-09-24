4aa9ebac1a253eb172797d2daa72f9cf11a05036
Alton Carroll <alton.carroll@outlook.com>
2021-09-23 23:53:42 -0500

    Couple more

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -393,7 +393,7 @@ namespace PatientPortalApp.Controllers
         public ActionResult LogOff()
             {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Landing", "Home");
             }
 
         //
